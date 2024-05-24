3982d07fa623c732c03e860c5f2c2c5afb7dd3b9
Cybernetics <mail@cybernetics.me>
2024-05-24 10:45:07 +0530

    small updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-12 col-md-12 last-paragraph-no-margin">
-                <h6 class="text-extra-dark-gray alt-font w-95 text-center mt-3">Get in Touch</h6>
+                <h6 class="text-extra-dark-gray alt-font w-95 mt-3">Get in Touch</h6>
                 <p class="w-85 lg-w-100 xl-w-95"><a href="mailto:contact@thecitizen.live" title="Get In Touch">contact@thecitizen.live</a></p>
             </div>
         </div>
